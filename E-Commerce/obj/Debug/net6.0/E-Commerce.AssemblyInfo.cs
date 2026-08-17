bcc3075ec5412f615ab799bee331bc7316d2ae05
OmarSameh12 <omarsamehmoh@gmail.com>
2026-08-18 02:02:15 +0300

    .net 6 version

--- E-Commerce/obj/Debug/net6.0/E-Commerce.AssemblyInfo.cs
+++ E-Commerce/obj/Debug/net6.0/E-Commerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+514e824f0c3979a8142c6a4ce2a3d4e9de2121a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
